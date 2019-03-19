494291926ee484e3c83abcdfaa66d87cec16daf8
Филиппов Алексей <filippovalex24@mail.ru>
2019-03-19 19:43:37 +0700

    create new project

---
+++ VFSimpleCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VFSimpleCalculator
+//
+//  Created by Филиппов Алексей on 19/03/2019.
+//  Copyright © 2019 BuboBubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
